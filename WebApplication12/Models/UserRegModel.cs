e49da3f42df3275e4865a34f5cbaf60496be65ec
nites <nites@DESKTOP-B7N39K8>
2023-10-29 07:30:19 +0545

    Add project files.

---
+++ WebApplication12/Models/UserRegModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication12.Models
+{
+    public class UserRegModel
+    {
+        [Required]
+        [DisplayName("User ID")]
+        public int id { get; set; }
+
+        [Required]
+        [DisplayName("Email ID")]
+        public string emailid { get; set; }
+
+        [Required]
+        [DisplayName("User Password")]
+        public string password { get; set; }
+
+        [Required]
+        [DisplayName("UserName")]
+        public string  name { get; set; }
+    }
+}
---
+++ WebApplication12/Repository/DAL.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.SqlTypes;
+
+namespace WebApplication12.Repository
+{
+    public class DAL
+    {
+        public SqlConnection con = new SqlConnection();
+    }
+}
